3fe36974689b8e88392273fc100c77324f3ecdc3
Christina Beckford <cbeckford2@brooklynsteamcenter.org>
2023-05-16 14:50:29 -0400

    added a file

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup>
   import {ref, watch} from 'vue'
 
-  let todos = ref(JSON.parse(window.localStorage.getItem('todos')))
+  let todos = ref(JSON.parse(window.localStorage.getItem('todos')) ?? [])
   let newInput =ref('')
 
   watch(todos, function(value) {
@@ -25,43 +25,65 @@ function deleteItem (index) {
 <template>
   <h1>My todo application</h1>
   <ul>
-  <li v-for="(todo, index) in todos">
+  <li v-for="(todo) in todos" :class="{completed: todo.complete}">
     <label class="container"></label>
     <input type="checkbox" id="checked" v-model="todo.complete">
     <label for="check"></label>
-    <button @click = "deleteItem (index)">💣</button>
+    <button @click = "deleteItem" id="bomb">❌</button>
     {{todo.text}}
   </li>
     </ul>
 
     <input v-model="newInput" @keydown.enter="input">
-    <button @click=input>add todo</button>
+    <button @click=input id="add">add todo</button>
 
   </template>
 
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Yomogi&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Englebert&display=swap');
 body{
-  font-family: 'Yomogi', cursive;
-  margin: auto;
-  max-width: 500px;
+  font-family: 'Englebert', sans-serif;
+  display: flex;
+  justify-content: center;
   background-color: #F1DEC9;
+  margin: 300px;
 }
 h1{
   color:#9F8772;
   text-decoration: underline;
-  text-align: center;
 }
 @import url('https://fonts.googleapis.com/css2?family=Neucha&display=swap');
-button{
-  border-radius: 6px;
+#add{
+  border-radius: 50%;
+  background-color: #FFE15D;
+  border-color: #B3C99C;
+  border-style: outset;
+  font-family: 'Neucha';
+  cursor: pointer;
+  padding: 25px;
+  box-shadow: 0 9px #999;
+}
+#add:active{
+  background-color: #B3C99C;
+  box-shadow: 0 5px #666;
+  transform: translateY(4px);
+}
+#bomb{
+  border-radius: 50%;
   background-color: #FFE15D;
   border-color: #B3C99C;
   border-style: dotted;
   font-family: 'Neucha';
+  cursor: pointer;
+  padding: 10px;
 }
 input{
   border-radius: 10px;
+  padding: 25px;
+  border-style: outset;
+  border-color: #B3C99C;
+  font-size: medium;
+  
 }
 input[type="checkbox"]{
   height: 20px;
@@ -69,7 +91,9 @@ input[type="checkbox"]{
   border-radius: 8px;
   cursor:pointer;
 }
-
-  
+.completed{
+  text-decoration: line-through;
+  color: #5D9C59;
+}
 
 </style>
